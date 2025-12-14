b152817ad5e7828737bfc030000a27f6b2eb5dca
Mehmet T <98125755+Eycest@users.noreply.github.com>
2025-12-14 23:50:26 +0300

    Update surec.py

--- isletim_odevi/src/surec.py
+++ isletim_odevi/src/surec.py
@@ -1,7 +1,10 @@
 class Surec:
-    def __init__(self, pid, gelis, sure, oncelik):
-        self.pid = pid
-        self.gelis = gelis
-        self.sure = sure
-        self.kalan = sure
-        self.oncelik = oncelik
+    def __init__(self, id, g, s, o):
+        self.id = id
+        self.g = g
+        self.s = s
+        self.k = s
+        self.o = o
+
+    def sozluk(self):
+        return {"id": self.id, "g": self.g, "s": self.s, "k": self.k, "o": self.o}
